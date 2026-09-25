8e1779aab48cf0259c466a79c464cd2c3ada768b
Sreya.s <sreyastephen09@gmail.com>
2026-09-25 11:28:00 +0530

    Add files via upload

---
+++ DAY2-FINDOUT/area-find.py
@@ -0,0 +1,12 @@
+r = 5
+circle = int(3.14 * r * r)
+print("Area of Circle =", circle)
+
+l = 10
+b = 5
+rectangle = l * b
+print("Area of Rectangle =", rectangle)
+
+s = 4
+square = s * s
+print("Area of Square =", square)
